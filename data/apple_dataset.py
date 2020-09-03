2c4a6214498de23603c271e8398315ec5c0d6557
nalingiri <56997222+nalingiri@users.noreply.github.com>
2020-09-03 19:55:48 +0530

    Add files via upload

---
+++ data/apple_dataset.py
@@ -0,0 +1,101 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import cv2
+#####################################
+# Class that takes the input instance masks
+# and extracts bounding boxes on the fly
+#####################################
+class AppleDataset(object):
+    def __init__(self, root_dir, transforms):
+        self.root_dir = root_dir
+        self.transforms = transforms
+
+        # Load all image and mask files, sorting them to ensure they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root_dir, "images"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root_dir, "masks"))))
+
+    def __getitem__(self, idx):
+        # Load images and masks
+        img_path = os.path.join(self.root_dir, "images", self.imgs[idx])
+        mask_path = os.path.join(self.root_dir, "masks", self.masks[idx])
+
+        img = Image.open(img_path).convert("RGB")
+        mask = cv2.imread(mask_path, 0)
+        #mask = Image.open(mask_path)#.convert('LA')     # Each color of mask corresponds to a different instance with 0 being the background
+        # 1.Convert('LA') is used for Color to Gray image
+        # 2.Originally it was not there.
+        # Convert the PIL image to np array
+        #===========================
+        mask = np.array(mask)
+        # print('==========')
+        # print('mask=',mask)
+        obj_ids = np.unique(mask)
+        # print('==========')
+        # print('obj_ids.shape=',obj_ids.shape)
+        # print('==========')
+        # print('obj_ids=',obj_ids)
+        # print('==========')
+        # Remove background id
+        obj_ids = obj_ids[1:]
+        # print('obj_ids=',obj_ids)
+        # print('==========')
+        #===========================
+        # Split the color-encoded masks into a set of binary masks
+        masks = mask == obj_ids[:, None, None]
+        # Get bbox coordinates for each mask
+        num_objs = len(obj_ids)
+        # print(num_objs)
+        boxes = []
+        # print('==========')
+        # print(mask.shape)
+        # print('==========')
+        h, w = mask.shape
+        for ii in range(num_objs):
+            pos = np.where(masks[ii])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+
+            if xmin == xmax or ymin == ymax:
+                continue
+
+            xmin = np.clip(xmin, a_min=0, a_max=w)
+            xmax = np.clip(xmax, a_min=0, a_max=w)
+            ymin = np.clip(ymin, a_min=0, a_max=h)
+            ymax = np.clip(ymax, a_min=0, a_max=h)
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # Convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        # There is only one class (apples)
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # All instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def get_img_name(self, idx):
+        return self.imgs[idx]
